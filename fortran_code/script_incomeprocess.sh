eb8b019562e52e9941268c1b3e57e7f4942e586f
Job Boerma <boerm002@umn.edu>
2016-12-04 19:27:51 -0600

    Server Script Test

--- fortran_code/script_incomeprocess.sh
+++ fortran_code/script_incomeprocess.sh
@@ -18,4 +18,6 @@ echo "Compiling"
 #ifort -m64 -g -debug all -heap-arrays nrtype.f90 stateControl.f90 genericParams.f90 simplex.f90 global.f90 objective_griewank.f90 utilities.f90 minimize.f90 GlobalSearch.f90 -o GlobalSearch
 gfortran -O3               nrtype.f90 myParams.f90 stateControl.f90 genericParams.f90 utilities.f90 simplex.f90 objective.f90 minimize.f90 GlobalSearch.f90 -o GlobalSearch
 
+echo "Compiling"
+
 ./GlobalSearch 0 config.txt a
